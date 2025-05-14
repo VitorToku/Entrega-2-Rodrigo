804e7c6136fd38cf4bbeffe9a761d4227fbabc7c
Vitor <vitor.htokunaga@gmail.com>
2025-05-13 22:37:11 -0300

    Subida do modelo Híbrido

--- predicao.py
+++ predicao.py
@@ -5,11 +5,13 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import json
+import pytz
 
 app = Flask(__name__)
 loaded = load("meu_modelo_treinado.joblib")
-encoder = loaded['encoder']
-modelo = loaded['model']
+
+modelo_linear = loaded['model_linear']
+modelo_rf = loaded['modelo_rf']
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -28,13 +30,29 @@ def prever():
 
 @app.route("/calcularCorrida", methods=['GET'])
 def get_precoCorrida():
-    agora = datetime.now()
+    agora = datetime.now(pytz.timezone('America/Sao_Paulo'))
+
+    listaCategorias = {
+        3: {
+            1: "Comfort",
+            2: "UberX",
+            4: "Black",
+            5: "Táxi Promo",
+            10: "Flash"
+        },
+        2 :  {
+            1: "Plus",
+            2: "Pop",
+            5: "Táxi",
+            6: "Top",
+        }
+    }
+
     origem = request.args.get('origem')
     print(f'Origem: {origem}')
     destino = request.args.get('destino')
     print(f'Destino: {destino}')
 
-    print(encoder.classes_)
     api_key = "AIzaSyCDmnx17lJCCO7GMJEIlqeBlRjnHxfI8b8"
     rota = f"https://maps.googleapis.com/maps/api/distancematrix/json?destinations={destino}&origins={origem}&key={api_key}"
 
@@ -44,27 +62,50 @@ def get_precoCorrida():
     distancia = jsonRota['rows'][0]['elements'][0]['distance']['value']
 
     duracao_segundos = jsonRota['rows'][0]['elements'][0]['duration']['value']
-    duracao_minutos = duracao_segundos // 60
 
-    codigos_categorias = list(range(len(encoder.classes_)))
+    hora_atual = agora.hour
+    dia = agora.day
+    mes = agora.month
+    ano = agora.year
+    eh_fim_de_semana = agora.weekday() in [6,7]
+    numero_dia_semana = agora.weekday()
+
     precos_por_categoria = {}
+    if(distancia > 15000):
+        model = modelo_linear
+        nome = "Linear Regression"
+    else:
+        model = modelo_rf
+        nome = "Random Forest"
 
-    for codigo in codigos_categorias:
-        entrada = pd.DataFrame([{
-            'distancia_m': distancia,
-            'tempo_estim_minutos': duracao_minutos,
-            'ProductID_encoded': codigo,
-            'dia': agora.day - 1,
-            'mes': agora.month,
-            'e_fim_de_semana': agora.weekday() >= 5
-        }])
-        pred = modelo.predict(entrada)[0]
-        nome_categoria = encoder.inverse_transform([codigo])[0]
-        precos_por_categoria[nome_categoria] = round(pred, 2)
+    print(f'--------------------{nome}----------------------------')
+    for id_empresa,categorias in listaCategorias.items():
+        for id_categoria, categoria in categorias.items():
 
+            entrada = pd.DataFrame([{
+                'id_prestador': id_empresa,
+                'id_categoria': id_categoria,
+                'dia': dia,
+                'mes': mes,
+                'ano': ano,
+                'eh_fim_de_semana':eh_fim_de_semana,
+                'hora': hora_atual,
+                'numero_dia_semana': numero_dia_semana,
+                'distancia_m': distancia,
+                'duracao_s': duracao_segundos,
+            }])
 
-    precos_ordenados = dict(sorted(precos_por_categoria.items(), key=lambda item: item[1]))
-    json_precos = json.dumps(precos_ordenados, ensure_ascii=False, indent=2)
+        pred = model.predict(entrada)[0]
+
+        nome_empresa = "99" if id_empresa == 2 else "Uber"
+      
+
+        if nome_empresa not in precos_por_categoria:
+            precos_por_categoria[nome_empresa] = {}
+
+        precos_por_categoria[nome_empresa][categoria] = round(pred, 2)
+    
+        json_precos = json.dumps(precos_por_categoria, ensure_ascii=False, indent=2)
     return json_precos
 
 
